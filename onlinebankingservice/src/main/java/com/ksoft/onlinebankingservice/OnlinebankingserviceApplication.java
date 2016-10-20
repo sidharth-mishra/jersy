eb4fb9fadf914f7ca29bdb58d6a7d91a476bb47d
sidharth-mishra <mishrasidhartha4@gmail.com>
2016-10-20 23:28:19 +0530

    initial version

---
+++ onlinebankingservice/src/main/java/com/ksoft/onlinebankingservice/OnlinebankingserviceApplication.java
@@ -0,0 +1,12 @@
+package com.ksoft.onlinebankingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebankingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebankingserviceApplication.class, args);
+	}
+}
